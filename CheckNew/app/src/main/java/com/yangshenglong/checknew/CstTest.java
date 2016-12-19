b91cbf417ee5aef6af64c9b344dc9b6d83d6d7e2
ChiSongTing <748233223@qq.com>
2016-12-19 11:22:43 +0800

    cut

---
+++ CheckNew/app/src/main/java/com/yangshenglong/checknew/CstTest.java
@@ -0,0 +1,12 @@
+package com.yangshenglong.checknew;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by CST on 16/12/19.
+ */
+
+public class CstTest extends Fragment{
+
+    // cst
+}
